4dde7d8f07d632c860b8456b26a447a1155d64fc
hit7sh <hit7sh@gmail.com>
2023-08-20 23:56:57 +0530

    updated mongo

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use('/admin', adminRouter);
 app.use('/user', userRouter);
 
-mongoose.connect('mongodb+srv://mongouser:mongo@cluster0.krn4y5n.mongodb.net/',
-   { useNewUrlParser: true, useUnifiedTopology: true, dbName: "CourseApp" });
+mongoose.connect('mongourl',
+  { useNewUrlParser: true, useUnifiedTopology: true, dbName: "CourseApp" });
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
